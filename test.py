606ff5137fd1d5562a237455549271fa99fe8de0
Aaron Schnider <aaron.schnider@gmail.com>
2021-06-10 06:34:11 -0500

    Added write to CSV

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source = requests.get('http://coreyms.com').text
 
 soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('cms_scrape.csv', 'w', newline='')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video_link'])
+
+
 
 for article in soup.find_all('article'):
     headline = article.h2.a.text
@@ -25,3 +33,7 @@ for article in soup.find_all('article'):
     print(yt_link)
 
     print()
+
+    csv_writer.writerow([headline, summary, yt_link])
+
+csv_file.close()
